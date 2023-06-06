f9ec9b50f2b3dd3be606727edaac5899abf69ee4
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-06-06 12:16:37 -0400

    finished-phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+function findMatching(drivers, query) {
+  return drivers.filter(driver =>
+    driver.toLowerCase() === query.toLowerCase()
+  );
+}
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver =>
+    driver.toLowerCase().startsWith(query.toLowerCase())
+  );
+}
+function matchName(drivers, query) {
+  return drivers.filter(driver =>
+    driver.name.toLowerCase() === query.toLowerCase()
+  );
+}
+const drivers = [
+  "John",
+  "Mary",
+  "Alice",
+  "Joel",
+  "Mark",
+];
+
+console.log(findMatching(drivers, "mary")); // Output: ["Mary"]
+console.log(fuzzyMatch(drivers, "jo")); // Output: ["John", "Joel"]
